ab02537dea1cbbfd9c1ca7c4c983bb0daa5a4c61
Hoàng Minh Đức <71325886+ducbadatcs@users.noreply.github.com>
2024-11-18 17:51:12 +0700

    changed columns

--- settings.php
+++ settings.php
@@ -4,39 +4,42 @@ $host = "feenix-mariadb.swin.edu.au";
 $user = "s104773652";
 $pwd = "hth233kiet";
 $sql_db = "s104773652_db";
-
-
+$table_name = "`eoi`";
 
 // test
 $test_local = true;
 
 if ($test_local) {
-    $host = "localhost";
-    $user = "root";
-    $pwd = "";
-    $sql_db = "test";
+	$host = "localhost";
+	$user = "root";
+	$pwd = "";
+	$sql_db = "test";
+	$table_name = "`eoi`";
 }
-
 
 $conn = mysqli_connect($host, $user, $pwd, $sql_db);
 
 if (!$conn) {
-    echo "<p>Connection Failed, please return!</p>";
+	echo "<p>Connection Failed, please return!</p>";
 } else {
-    $tableQuery = "SHOW TABLES LIKE 'accounts'";
-    $tablecheck = mysqli_query($conn, $tableQuery);
-
-    if (mysqli_num_rows($tablecheck) == 0) {
-        // Table does not exist, create it
-        $createTableQuery = "CREATE TABLE `accounts` (
-                `id` INT(255) AUTO_INCREMENT PRIMARY KEY,
-                `username` VARCHAR(255) NOT NULL,
-                `password` VARCHAR(255) NOT NULL
-            )";
-        mysqli_query($conn, $createTableQuery);
-
-        // Insert sample data
-        $insertQuery = "INSERT INTO `accounts` (`id`, `username`, `password`) VALUES ('0', 'admin', '$hashedPassword')";
-        mysqli_query($conn, $insertQuery);
-    }
+	// Create table 
+	$createTableQuery = "CREATE TABLE `eoi` (
+	`eoinumber` int(11) NOT NULL,
+  `job_ref` char(5) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `dob` date NOT NULL,
+  `address` varchar(40) NOT NULL,
+  `suburbtown` varchar(40) NOT NULL,
+  `state` enum('VIC','NSW','QLD','NT','WA','SA','TAS','ACT') NOT NULL,
+  `post` char(4) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `html` tinyint(1) NOT NULL,
+  `css` tinyint(1) NOT NULL,
+  `js` tinyint(1) NOT NULL,
+  `php` tinyint(1) NOT NULL,
+  `mysql` tinyint(1) NOT NULL,
+  `other_skills` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+	mysqli_query($conn, $createTableQuery);
 }
